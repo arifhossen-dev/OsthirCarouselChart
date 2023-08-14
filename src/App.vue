a832f85ce49f1d5c96f6a10ebf867aa616cd949d
ahakbsl <ahak.bsl@gmail.com>
2023-08-14 18:57:36 +0600

    error update and typo fixed

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,13 @@ const toggleCarousel = ref(true)
 </script>
 
 <template>
-  <button>Toggle Carouser</button>
   <div class="mx-auto mt-10 max-w-7xl">
     <Chart/>
 
     <div>
-      <div class="bg-green-500 mt-5 text-center py-5">
+      <div class="py-5 mt-5 text-center bg-green-500">
         <button @click="toggleCarousel = !toggleCarousel" type="button"
-          class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200">
+          class="inline-flex items-center px-5 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200">
           Click to Toggle The Carousel and check in console
         </button>
       </div>
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -19,11 +19,12 @@ onMounted(() => {
   chart = new Chart(ctx, config)
 })
 
-const randColor = () =>  {
-    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+const randColor = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0').toUpperCase();
 }
 
 function updateChart() {
+  alert('some test')
   if (!newLabel.value) {
     error.value = true
   }
@@ -50,22 +51,26 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <section class="p-10 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 rounded-3xl my-10">
+  <section class="p-10 my-10 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 rounded-3xl">
     <div class="flex flex-col items-center">
       <div class=" mx-auto w-[400px] h-[400px] bg-white/60 p-5 rounded-xl">
         <canvas id="chart">
         </canvas>
       </div>
 
-      <div class="mt-10 flex items-center gap-3">
-        <input type="text" class="rounded-lg outline-none focus:outline-none py-3"
-          :class="error ? 'border bg-red-400 border-red-600' : 'border-none'" required v-model="newLabel">
-        <input type="number" class="rounded-lg border-none outline-none focus:outline-none py-3" v-model="newItem">
+      <div class="mt-10">
+        <p v-show="error" class="self-start px-2 my-1 text-red-600 bg-yellow-300">Chart label field can't be null</p>
+      <div class="flex items-center gap-3">
+        <input type="text" class="py-3 rounded-lg outline-none focus:outline-none"
+          :class="error ? 'border bg-yellow-400 border-yellow-600' : 'border-none'" placeholder="Enter chart label"
+          v-model="newLabel">
+        <input type="number" class="py-3 border-none rounded-lg outline-none focus:outline-none" v-model="newItem">
 
 
       </div>
+      </div>
       <button @click="updateChart()" type="button"
-        class="mt-5 text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-800 dark:bg-white dark:border-gray-700 dark:text-gray-900 dark:hover:bg-gray-200">
+        class="inline-flex items-center px-5 py-2 mt-5 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100">
         <svg class="w-8 mr-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle opacity="0.5" cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5" />
           <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1C274C" stroke-width="1.5"
